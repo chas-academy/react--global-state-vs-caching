bce5df4fe188b5f7d3fe6d9d4e58cda072a7b183
Fredrik Karlsson <fredrikkarlsson320@gmail.com>
2026-01-30 09:31:16 +0100

    trim the hedges

--- src/lib/zustand-store.ts
+++ src/lib/zustand-store.ts
@@ -7,24 +7,8 @@ interface Boats {
   setBoats: (boats: number) => void;
 }
 
-export const useBoats = create<Boats>((set) => ({
-  boats: 0,
-  setBoats: (boats: number) => set({ boats }),
-}));
-
 interface UsePokemon {
   pokemon: Pokemon | undefined;
   isLoadingPokemon: boolean;
   setPokemon: (species: string) => Promise<void>;
 }
-
-export const usePokemon = create<UsePokemon>((set) => ({
-  pokemon: undefined,
-  isLoadingPokemon: false,
-  setPokemon: async (species: string) => {
-    set({ isLoadingPokemon: true });
-    const pokemon = await getPokemon(species);
-    set({ pokemon });
-    set({ isLoadingPokemon: false });
-  },
-}));
